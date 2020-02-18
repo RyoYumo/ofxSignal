402681d54ab035d58cec52566193de9d8cb2f187
ryoyumo <yumo.after.fun@gmail.com>
2020-02-18 15:05:43 +0900

    update

--- src/ofxSignal.h
+++ src/ofxSignal.h
@@ -2,34 +2,30 @@
 #define OFX_SIGNAL_H_
 
 #include <functional>
+#include <vector>
 #include "ofEvents.h"
 
 namespace ofx{
 template<typename T>
 class Signal {
 public:
-    Signal(){
-        ofAddListener(event_, this, &Signal::call);
-    }
-    ~Signal(){
-        ofRemoveListener(event_, this, &Signal::call);
-    }
+    using CallbackType = std::function<void(T&)>;
+    Signal(){ ofAddListener(event_, this, &Signal::call);}
+    ~Signal(){ofRemoveListener(event_, this, &Signal::call);}
+    
     template<typename Callback>
     void connect(Callback&& callback){
-        callback_obj_ = std::forward<Callback>(callback);
+        callback_collection_.push_back(std::forward<Callback>(callback));
     }
     
-    void emit(T& arg){
-        ofNotifyEvent(event_, arg);
-    }
-    
+    void emit(T& arg){ ofNotifyEvent(event_, arg);}
     
 private:
     ofEvent<T> event_;
-    std::function<void(T&)> callback_obj_;
+    std::vector<CallbackType> callback_collection_;
     void call(T& arg){
-        if(callback_obj_){
-            callback_obj_(arg);
+        for(auto& c : callback_collection_) {
+            if(c) c(arg);
         }
     }
 };
@@ -37,28 +33,22 @@ private:
 template<>
 class Signal<void> {
 public:
-    Signal(){
-        ofAddListener(event_, this, &Signal::call);
-    }
-    ~Signal(){
-        ofRemoveListener(event_, this, &Signal::call);
-    }
+    using CallbackType = std::function<void(void)>;
+    Signal(){ ofAddListener(event_, this, &Signal::call);}
+    ~Signal(){ ofRemoveListener(event_, this, &Signal::call);}
     
     template<typename Callback>
     void connect(Callback&& callback){
-        callback_obj_ = std::forward<Callback>(callback);
+        callback_collection_.push_back(std::forward<Callback>(callback));
     }
     
-    void emit(){
-        ofNotifyEvent(event_);
-    }
-    
+    void emit(){ ofNotifyEvent(event_);}
 private:
     ofEvent<void> event_;
-    std::function<void(void)> callback_obj_;
+    std::vector<CallbackType> callback_collection_;
     void call(){
-        if(callback_obj_){
-            callback_obj_();
+        for(auto& c : callback_collection_) {
+            if(c) c();
         }
     }
 };
@@ -66,11 +56,6 @@ private:
 
 template<typename T>
 using ofxSignal = ofx::Signal<T>;
-
-using ofxSignalVoid  = ofxSignal<void>;
-using ofxSignalInt   = ofxSignal<int>;
-using ofxSignalFloat = ofxSignal<float>;
-using ofxSignalVec3  = ofxSignal<glm::vec3>;
 
 
 #endif // OFX_SIGNAL_H_
